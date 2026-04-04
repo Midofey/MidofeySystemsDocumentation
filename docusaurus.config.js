c25e8c51a013cec43be5eb9a1637a3a0782394f7
Midofey <midofey.official@gmail.com>
2026-04-04 23:51:02 +0200

    feature: Added Search Bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,6 +130,15 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+  themes: [
+        [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+        ({
+          hashed: true,
+        }),
+    ],
+  ],
 };
 
 export default config;
